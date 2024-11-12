6e688c262ddfebd5298f83f9cf71d2be18f01d41
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2024-11-12 17:09:57 +0800

    feat: cleanup (#165)
    * feat: remove unneccessary masApps
    
    * feat: remove homerow app

--- modules/apps.nix
+++ modules/apps.nix
@@ -44,10 +44,10 @@
     # For details, see https://github.com/mas-cli/mas 
     masApps = {
       # --- when want to use safari ---
-      "Wappalyzer" = 1520333300;
+      # "Wappalyzer" = 1520333300;
       "AdGuard for Safari" = 1440147259;
       # "Consent-O-Matic" = 1606897889;
-      "JSON Peep for Safari" = 1458969831;
+      # "JSON Peep for Safari" = 1458969831;
       # "Vimlike" = 1584519802;
 
       # --- misc ---
@@ -81,7 +81,7 @@
       "appcleaner"
       "pronotes"
       "hammerspoon" # mainly used for safari vim mode
-      "homerow"
+      # "homerow"
 
       # --- docker desktop alternative ---
       "orbstack"
