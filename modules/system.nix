589f42b37ef606ab09b2eb573bdde354997b1a83
y3owk1n <wongyeowkin@gmail.com>
2024-11-30 16:31:11 +0800

    fix: nerd font api changes for nix darwin

--- modules/system.nix
+++ modules/system.nix
@@ -991,19 +991,8 @@ in
   fonts = {
     packages = with pkgs; [
       poppins
-
-      # nerdfonts
-      # https://github.com/NixOS/nixpkgs/blob/nixos-24.05/pkgs/data/fonts/nerdfonts/shas.nix
-      (nerdfonts.override {
-        fonts = [
-          # symbols icon only
-          "NerdFontsSymbolsOnly"
-          # Characters
-          # "FiraCode"
-          "JetBrainsMono"
-          # "Iosevka"
-        ];
-      })
+      nerd-fonts.symbols-only
+      nerd-fonts.jetbrains-mono
     ];
   };
 }
