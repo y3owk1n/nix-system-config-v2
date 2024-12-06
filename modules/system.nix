2806edd0229b3ed06f145b5b26e0863b53d30fa9
mtraworld <140996996+mtraworld@users.noreply.github.com>
2024-12-06 17:57:49 +0800

    feat: make nix darwin & home manager apps to index by spotlight (#225)
    Co-authored-by: mtraworld <kylewong@traworld.com>

--- modules/system.nix
+++ modules/system.nix
@@ -50,15 +50,14 @@ in
       # so we do not need to logout and login again to make the changes take effect.
       /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
 
-      ## NOTE: Do not delete this! Uncomment this when you want to use spotlight search
-      ## This will enable spotlight search to index installed apps
-      # rsyncArgs="--archive --checksum --chmod=-w --copy-unsafe-links --delete"
-      # apps_source="${config.system.build.applications}/Applications"
-      # moniker="Nix Trampolines"
-      # app_target_base="$HOME/Applications"
-      # app_target="$app_target_base/$moniker"
-      # mkdir -p "$app_target"
-      # ${pkgs.rsync}/bin/rsync $rsyncArgs "$apps_source/" "$app_target"
+      # NOTE: Do not delete this! Uncomment this when you want to use spotlight search
+      # This will enable spotlight search to index installed apps
+      apps_source="${config.system.build.applications}/Applications"
+      moniker="Nix Trampolines"
+      app_target_base="$HOME/Applications"
+      app_target="$app_target_base/$moniker"
+      mkdir -p "$app_target"
+      ${pkgs.rsync}/bin/rsync --archive --checksum --chmod=-w --copy-unsafe-links --delete "$apps_source/" "$app_target"
     '';
 
     defaults = {
