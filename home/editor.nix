d04d8db1dc0f6968685b15224c40949bc1c098f1
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2025-02-02 21:44:06 +0800

    feat(nix): add global editorconfig (#484)

--- home/editor.nix
+++ home/editor.nix
@@ -16,4 +16,19 @@
     source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-system-config-v2/config/nvim";
     recursive = true;
   };
+
+  editorconfig = {
+    enable = true;
+    settings = {
+      "*" = {
+        charset = "utf-8";
+        end_of_line = "lf";
+        indent_size = 4;
+        indent_style = "tab";
+        insert_final_newline = true;
+        trim_trailing_whitespace = true;
+        max_line_width = 120;
+      };
+    };
+  };
 }
