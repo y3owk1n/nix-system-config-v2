a91c24c9cdd1e4b691d574b9eff7ceea561ad418
y3owk1n <wongyeowkin@gmail.com>
2025-02-15 01:01:52 +0800

    feat: bump nix deps

--- modules/nix-core.nix
+++ modules/nix-core.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 
 {
   # enable flakes globally
@@ -19,14 +19,14 @@
   # don't wan't the daemon service to be managed for you.
   # nix.useDaemon = true;
 
-  # nix.package = pkgs.nix;
+  nix.package = pkgs.nix;
   # nix.package = pkgs.nixVersions.git;
 
   # do garbage collection weekly to keep disk usage low
-  nix.gc = {
-    automatic = lib.mkDefault true;
-    options = lib.mkDefault "--delete-older-than 7d";
-  };
+  # nix.gc = {
+  #   automatic = lib.mkDefault true;
+  #   options = lib.mkDefault "--delete-older-than 7d";
+  # };
 
   # Disable auto-optimise-store because of this issue:
   #   https://github.com/NixOS/nix/issues/7273
