9147df33b8ed1c3831793a92ca89b8b3b0fbeaeb
mtraworld <140996996+mtraworld@users.noreply.github.com>
2025-02-18 12:35:57 +0800

    feat: add skhd for yabai (it's faster than hammerspoon) (#529)

--- modules/yabai.nix
+++ modules/yabai.nix
@@ -44,5 +44,31 @@
         yabai -m rule --add app="^(Finder|System Settings|Archive Utility|Creative Cloud|Logi Options|FaceTime)$" manage=off
       '';
     };
+    skhd = {
+      enable = true;
+      package = pkgs.skhd;
+      skhdConfig = ''
+        cmd + shift + alt + ctrl - h : yabai -m window --focus west
+        cmd + shift + alt + ctrl - j : yabai -m window --focus south
+        cmd + shift + alt + ctrl - k : yabai -m window --focus north
+        cmd + shift + alt + ctrl - l : yabai -m window --focus east
+
+        # swap managed window
+        ctrl + shift - h : yabai -m window --swap west
+        ctrl + shift - j : yabai -m window --swap south
+        ctrl + shift - k : yabai -m window --swap north
+        ctrl + shift - l : yabai -m window --swap east
+
+        # toggle window fullscreen zoom
+        cmd + shift + alt + ctrl - m : yabai -m window --toggle zoom-fullscreen
+
+        # float / unfloat window and center on screen
+        cmd + shift + alt + ctrl - t : yabai -m window --toggle float;\
+                  yabai -m window --grid 4:4:1:1:2:2
+
+        # balance size of windows
+        cmd + shift + alt + ctrl - 0 : yabai -m space --balance
+      '';
+    };
   };
 }
