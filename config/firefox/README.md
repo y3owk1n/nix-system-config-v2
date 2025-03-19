1599aa05a53dccd4ac6f54aec0373cc1897fa9b1
mtraworld <140996996+mtraworld@users.noreply.github.com>
2025-03-19 17:26:16 +0800

    feat: switch to zen browser (#593)

--- config/firefox/README.md
+++ config/firefox/README.md
@@ -1,3 +1,13 @@
-# Betterfox
+# Firefox / Zen browser
+
+## Betterfox
 
 [Github for betterfox](https://github.com/yokoffing/Betterfox)
+
+## Plugins
+
+- iCloud Passwords
+- Pin Unpin Tab
+- React Developer Tools
+- uBlock Origin
+- Vimium C - All by keyboard
--- config/hammerspoon/init.lua
+++ config/hammerspoon/init.lua
@@ -6,7 +6,7 @@
 -- require("neovim")
 
 -- local menubarSpace = require("menubar-space") -- show the current active space in number
-local vimium = require("vimium") -- best effort to have vimium system wide
+-- local vimium = require("vimium") -- best effort to have vimium system wide
 
-vimium:start()
+-- vimium:start()
 -- menubarSpace:start()
