80c8657eb3f64a6965d0ea587966cd8fb391d8cc
mtraworld <140996996+mtraworld@users.noreply.github.com>
2025-04-02 11:16:24 +0800

    fix: remove rsync for nix-apps (#638)

--- modules/system.nix
+++ modules/system.nix
@@ -51,12 +51,12 @@ in
 
       # NOTE: Do not delete this! Uncomment this when you want to use spotlight search
       # This will enable spotlight search to index installed apps
-      apps_source="${config.system.build.applications}/Applications"
-      moniker="Nix Trampolines"
-      app_target_base="$HOME/Applications"
-      app_target="$app_target_base/$moniker"
-      mkdir -p "$app_target"
-      ${pkgs.rsync}/bin/rsync --archive --checksum --chmod=-w --copy-unsafe-links --delete "$apps_source/" "$app_target"
+      # apps_source="${config.system.build.applications}/Applications"
+      # moniker="Nix Trampolines"
+      # app_target_base="$HOME/Applications"
+      # app_target="$app_target_base/$moniker"
+      # mkdir -p "$app_target"
+      # ${pkgs.rsync}/bin/rsync --archive --checksum --chmod=-w --copy-unsafe-links --delete "$apps_source/" "$app_target"
     '';
 
     defaults = {
