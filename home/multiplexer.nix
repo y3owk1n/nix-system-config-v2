6e793f96d0ccfccbaf4e5cacc97a0f3a02074df8
mtraworld <140996996+mtraworld@users.noreply.github.com>
2025-04-11 14:37:00 +0800

    feat(tmux): add support for quick tmux popup with dotmd integration (#677)

--- home/multiplexer.nix
+++ home/multiplexer.nix
@@ -86,6 +86,13 @@
 
         bind-key -r b run-shell "bash ~/nix-system-config-v2/scripts/tmux-sessionizer.sh"
 
+        bind-key C-n switch-client -T dotmd
+        bind-key -T dotmd t run-shell "tmux popup -E -w 90% -h 80% -T 'Dotmd Todo' 'sh -c \"cd ~/Library/Mobile\\ Documents/com~apple~CloudDocs/Cloud\\ Notes && nvim +\\\"DotMdCreateTodoToday split=none\\\"\"'"
+        bind-key -T dotmd n run-shell "tmux popup -E -w 90% -h 80% -T 'Dotmd Note' 'sh -c \"cd ~/Library/Mobile\\ Documents/com~apple~CloudDocs/Cloud\\ Notes && nvim +\\\"DotMdCreateNote split=none\\\"\"'"
+        bind-key -T dotmd i run-shell "tmux popup -E -w 90% -h 80% -T 'Dotmd Inbox' 'sh -c \"cd ~/Library/Mobile\\ Documents/com~apple~CloudDocs/Cloud\\ Notes && nvim +\\\"DotMdInbox split=none\\\"\"'"
+        bind-key -T dotmd r run-shell "tmux popup -E -w 90% -h 80% -T 'Dotmd Root' 'sh -c \"cd ~/Library/Mobile\\ Documents/com~apple~CloudDocs/Cloud\\ Notes && nvim\"'"
+        bind-key -T dotmd Escape switch-client -T root
+
         bind-key -T copy-mode-vi 'v' send -X begin-selection # start selecting text with "v"
         bind-key -T copy-mode-vi 'y' send -X copy-selection # copy text with "y"
 
