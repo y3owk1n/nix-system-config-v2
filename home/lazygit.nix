2df136f35eadf4e2c6b4552f94a659daa928d9d6
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2025-05-11 20:00:41 +0800

    fix(lazygit): update nicer config (#813)
    * fix(lazygit): update nicer config
    
    Signed-off-by: Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
    
    * remove signoff
    
    ---------
    
    Signed-off-by: Kyle Wong <62775956+y3owk1n@users.noreply.github.com>

--- home/lazygit.nix
+++ home/lazygit.nix
@@ -3,6 +3,7 @@
   programs.lazygit = {
     enable = true;
     settings = {
+      disableStartupPopups = true;
       os.editPreset = "nvim-remote";
       gui = {
         nerdFontsVersion = "3";
@@ -23,7 +24,13 @@
             "bold"
           ];
         };
-
+        showNumstatInFilesView = true;
+        showRandomTip = false;
+      };
+      git = {
+        paging = {
+          externalDiffCommand = "difft --color=always";
+        };
       };
     };
   };
