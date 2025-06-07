4d55d76236cb6bf44e6242be97ff9ff21872800c
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2025-06-07 11:00:20 +0800

    fix(darwin): remove fnm and use direnv flake to manage node (#905)

--- home-manager/packages/fish.nix
+++ home-manager/packages/fish.nix
@@ -9,9 +9,9 @@
       __load-em
       __autols_hook
       starship_transient_prompt_func
-      if type -q fnm
-      	fnm env --use-on-cd --version-file-strategy=recursive --resolve-engines --shell fish | source
-      end
+      # if type -q fnm
+      # 	fnm env --use-on-cd --version-file-strategy=recursive --resolve-engines --shell fish | source
+      # end
     '';
     shellAliases = {
       gg = "lazygit";
