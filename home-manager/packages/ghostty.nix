4d8b9ec7e8ffb02e593b0a71e2af3e26a66c1203
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2025-07-11 09:31:05 +0800

    fix(nix): remove `recursive` for config symlink (#929)

--- home-manager/packages/ghostty.nix
+++ home-manager/packages/ghostty.nix
@@ -3,6 +3,6 @@
   xdg.configFile.ghostty = {
     enable = true;
     source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-system-config-v2/config/ghostty";
-    recursive = true;
+    # recursive = true;
   };
 }
