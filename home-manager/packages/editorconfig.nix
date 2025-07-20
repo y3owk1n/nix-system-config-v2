f8e83e22a9cb6dc1f6f875939c463c788194931d
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2025-07-20 22:41:41 +0800

    fix: update editorconfig for yml & justfile (#953)

--- home-manager/packages/editorconfig.nix
+++ home-manager/packages/editorconfig.nix
@@ -5,12 +5,23 @@
     settings = {
       "*" = {
         charset = "utf-8";
-        end_of_line = "lf";
-        indent_size = 4;
         indent_style = "tab";
+        tab_width = 4;
+        indent_size = 4;
+        end_of_line = "lf";
         insert_final_newline = true;
         trim_trailing_whitespace = true;
         max_line_width = 120;
+      };
+      "*.{yaml,yml}" = {
+        indent_style = "space";
+        tab_width = 2;
+        indent_size = 2;
+      };
+      "Justfile" = {
+        indent_style = "space";
+        tab_width = 2;
+        indent_size = 2;
       };
     };
   };
