0a9aedb9801d841586d9ab73d4f1975019baabd6
mtraworld <140996996+mtraworld@users.noreply.github.com>
2025-08-15 17:21:57 +0800

    feat: use my own custom `skhd` module instead of `nix-darwin`

--- darwin/modules/skhd.nix
+++ darwin/modules/skhd.nix
@@ -1,50 +1,55 @@
-{ pkgs, ... }:
-let
-  skhd-zig = pkgs.stdenv.mkDerivation rec {
-    pname = "skhd-zig";
-    version = "0.0.12";
-
-    src = pkgs.fetchzip {
-      url = "https://github.com/jackielii/skhd.zig/releases/download/v${version}/skhd-arm64-macos.tar.gz";
-      sha256 = "sha256-qJt2wWfM7YYVfWPbaGJ5w2LbWDhhN2MBvs+m0PCeLqM=";
-      stripRoot = false;
-    };
-
-    phases = [ "installPhase" ];
-
-    dontBuild = true; # nothing to compile
-
-    installPhase = ''
-      mkdir -p $out/bin
-      cp $src/skhd-arm64-macos $out/bin/skhd
-      chmod +x $out/bin/skhd
-    '';
-  };
-in
 {
-  # NOTE: Needs to re-grant accessibility permissions after hash updated
-  #
-  # ensure the service is running
-  # launchctl list | grep skhd
-  #
-  # use the following commands to restart the service or `just relaunch-skhd`
-  # launchctl bootout gui/$(id -u)/org.nixos.skhd && \
-  # launchctl bootstrap gui/$(id -u) ~/Library/LaunchAgents/org.nixos.skhd.plist
-  services.skhd = {
-    enable = true;
-    package = skhd-zig; # <-- Zig rewrite
-    skhdConfig = ''
-      # launchers
-      hyper - b : open -a Safari
-      hyper - t : open -a Ghostty
-      hyper - n : open -a Notes
-      hyper - m : open -a Mail
-      hyper - w : open -a WhatsApp
-      hyper - f : open ~
-      hyper - s : open -a "System Settings"
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
-      # spotlight
-      hyper - return : skhd --key "cmd - space"
-    '';
+let
+  cfg = config.skhd;
+
+  configFile = pkgs.writeScript "skhdrc" cfg.config;
+in
+
+{
+  options = {
+    skhd = with lib.types; {
+      enable = lib.mkEnableOption "Skhd (zig)";
+
+      package = lib.mkPackageOption pkgs "skhd" { };
+
+      config = lib.mkOption {
+        type = types.lines;
+        default = ''
+          # launchers
+          hyper - b : open -a Safari
+          hyper - t : open -a Ghostty
+          hyper - n : open -a Notes
+          hyper - m : open -a Mail
+          hyper - w : open -a WhatsApp
+          hyper - f : open ~
+          hyper - s : open -a "System Settings"
+
+          # spotlight
+          hyper - return : skhd --key "cmd - space"
+        '';
+        description = "Config to use for {file} `skhdrc`.";
+      };
+    };
   };
+
+  config = (
+    lib.mkIf (cfg.enable) {
+      environment.systemPackages = [ cfg.package ];
+
+      launchd.user.agents.skhd = {
+        command = "${cfg.package}/bin/skhd" + (lib.optionalString (cfg.config != "") " -c ${configFile}");
+        serviceConfig = {
+          KeepAlive = true;
+          RunAtLoad = true;
+          ProcessType = "Interactive";
+        };
+      };
+    }
+  );
 }
