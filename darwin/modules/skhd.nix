8ff7443251cb7fc726eda71ddf1544a5f701a242
mtraworld <140996996+mtraworld@users.noreply.github.com>
2025-08-15 16:52:38 +0800

    feat: use `skhd.zig` and better setup for skhd changes

--- darwin/modules/skhd.nix
+++ darwin/modules/skhd.nix
@@ -1,17 +1,40 @@
 { pkgs, ... }:
+let
+  skhd-zig = pkgs.stdenv.mkDerivation rec {
+    pname = "skhd-zig";
+    version = "0.0.12";
+
+    src = pkgs.fetchzip {
+      url = "https://github.com/jackielii/skhd.zig/releases/download/v${version}/skhd-arm64-macos.tar.gz";
+      sha256 = "sha256-qJt2wWfM7YYVfWPbaGJ5w2LbWDhhN2MBvs+m0PCeLqM=";
+      stripRoot = false;
+    };
+
+    phases = [ "installPhase" ];
+
+    dontBuild = true; # nothing to compile
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp $src/skhd-arm64-macos $out/bin/skhd
+      chmod +x $out/bin/skhd
+    '';
+  };
+in
 {
   # NOTE: Needs to re-grant accessibility permissions after hash updated
   #
   # ensure the service is running
   # launchctl list | grep skhd
   #
-  # use the following commands to restart the service
+  # use the following commands to restart the service or `just relaunch-skhd`
   # launchctl bootout gui/$(id -u)/org.nixos.skhd && \
   # launchctl bootstrap gui/$(id -u) ~/Library/LaunchAgents/org.nixos.skhd.plist
   services.skhd = {
     enable = true;
-    package = pkgs.skhd; # fixed path, won't change every rebuild
+    package = skhd-zig; # <-- Zig rewrite
     skhdConfig = ''
+      # launchers
       hyper - b : open -a Safari
       hyper - t : open -a Ghostty
       hyper - n : open -a Notes
@@ -19,6 +42,9 @@
       hyper - w : open -a WhatsApp
       hyper - f : open ~
       hyper - s : open -a "System Settings"
+
+      # spotlight
+      hyper - return : skhd --key "cmd - space"
     '';
   };
 }
