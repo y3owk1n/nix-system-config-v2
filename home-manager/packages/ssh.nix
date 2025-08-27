bd4f400ee42d840a10be69464f34695e65e0d6a9
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2025-08-27 08:13:50 +0800

    fix: update deprecated soon ssh config for hm

--- home-manager/packages/ssh.nix
+++ home-manager/packages/ssh.nix
@@ -2,7 +2,7 @@
 {
   programs.ssh = {
     enable = true;
-    addKeysToAgent = "yes";
+    enableDefaultConfig = false; # Explicitly enable default config to suppress warning
     includes = (
       if pkgs.stdenv.isDarwin then
         [
@@ -11,11 +11,30 @@
       else
         [ ]
     );
-    extraConfig = ''
-      Host github.com
-      	AddKeysToAgent yes
-      	${if pkgs.stdenv.isDarwin then "UseKeychain yes" else ""}
-      	IdentityFile ~/.ssh/id_ed25519
-    '';
+
+    matchBlocks = {
+      # Global defaults (replacing the deprecated top-level addKeysToAgent)
+      "*" = {
+        forwardAgent = false;
+        addKeysToAgent = "yes";
+        compression = false;
+        serverAliveInterval = 0;
+        serverAliveCountMax = 3;
+        hashKnownHosts = false;
+        userKnownHostsFile = "~/.ssh/known_hosts";
+        controlMaster = "no";
+        controlPath = "~/.ssh/master-%r@%h:%p";
+        controlPersist = "no";
+      };
+
+      # GitHub-specific configuration
+      "github.com" = {
+        addKeysToAgent = "yes";
+        identityFile = "~/.ssh/id_ed25519";
+        extraOptions = {
+          useKeychain = if pkgs.stdenv.isDarwin then "yes" else "no";
+        };
+      };
+    };
   };
 }
