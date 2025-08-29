02787f5161ffbec326fe05cc825711a835e53eac
mtraworld <140996996+mtraworld@users.noreply.github.com>
2025-08-29 11:48:16 +0800

    refactor: restructure init.lua code with proper `init` calls (#1226)

--- config/nvim/lua/autocmds.lua
+++ config/nvim/lua/autocmds.lua
@@ -1,249 +1,259 @@
+local M = {}
+
 local function augroup(name)
   return vim.api.nvim_create_augroup("k92_" .. name, { clear = true })
 end
 
-------------------------------------------------------------
--- Yank Highlight (Optional)
-------------------------------------------------------------
--- Currently highlight with `undo-glow.nvim`.
--- Only enable it when personal plugins are stripped.
-vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking (copying) text",
-  group = augroup("highlight_yank"),
-  callback = function()
-    local ok = pcall(require, "undo-glow")
-    if ok then
-      return
-    end
-    (vim.hl or vim.highlight).on_yank()
-  end,
-})
+local function setup_autocmds()
+  ------------------------------------------------------------
+  -- Yank Highlight (Optional)
+  ------------------------------------------------------------
+  -- Currently highlight with `undo-glow.nvim`.
+  -- Only enable it when personal plugins are stripped.
+  vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight when yanking (copying) text",
+    group = augroup("highlight_yank"),
+    callback = function()
+      local ok = pcall(require, "undo-glow")
+      if ok then
+        return
+      end
+      (vim.hl or vim.highlight).on_yank()
+    end,
+  })
 
-------------------------------------------------------------
--- Close Certain Filetypes with <q>
-------------------------------------------------------------
-vim.api.nvim_create_autocmd("FileType", {
-  group = augroup("close_with_q"),
-  pattern = {
-    "checkhealth",
-    "dbout",
-    "grug-far",
-    "help",
-    "qf",
-    "startuptime",
-    "tsplayground",
-    "mininotify-history",
-    "lspinfo",
-    "lsplog",
-    "lintinfo",
-    "cmd",
-    "nvim-pack",
-  },
-  callback = function(event)
-    -- Prevent the buffer from appearing in the buffer list.
-    vim.bo[event.buf].buflisted = false
-    vim.schedule(function()
-      -- Map <q> in the buffer to close it and delete the buffer.
-      vim.keymap.set("n", "q", function()
-        vim.cmd("close")
-        pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
-      end, {
-        buffer = event.buf,
-        silent = true,
-        desc = "Quit buffer",
-      })
-    end)
-  end,
-})
-
-------------------------------------------------------------
--- Open Buffer: Restore Last Cursor Location
-------------------------------------------------------------
-vim.api.nvim_create_autocmd("BufReadPost", {
-  group = augroup("last_loc"),
-  callback = function(event)
-    local exclude = { "gitcommit" }
-    local buf = event.buf
-    -- Skip if filetype is excluded or already processed.
-    if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].last_loc then
-      return
-    end
-    vim.b[buf].last_loc = true
-    local mark = vim.api.nvim_buf_get_mark(buf, '"')
-    local lcount = vim.api.nvim_buf_line_count(buf)
-    -- Jump to last cursor position if it's within file bounds.
-    if mark[1] > 0 and mark[1] <= lcount then
-      pcall(vim.api.nvim_win_set_cursor, 0, mark)
-    end
-  end,
-})
-
-------------------------------------------------------------
--- Remove Trailing Whitespace on Save
-------------------------------------------------------------
-vim.api.nvim_create_autocmd("BufWritePre", {
-  group = augroup("remove_whitespace_on_save"),
-  pattern = "",
-  -- Remove trailing whitespace before saving.
-  command = ":%s/\\s\\+$//e",
-})
-
-------------------------------------------------------------
--- Disable Auto-Commenting on New Lines
-------------------------------------------------------------
-vim.api.nvim_create_autocmd("BufEnter", {
-  group = augroup("no_auto_commeting_new_lines"),
-  pattern = "",
-  -- Remove automatic insertion of comment leaders on new lines.
-  command = "set fo-=c fo-=r fo-=o",
-})
-
-------------------------------------------------------------
--- Turn Off Paste Mode When Leaving Insert Mode
-------------------------------------------------------------
-vim.api.nvim_create_autocmd("InsertLeave", {
-  group = augroup("paste_mode_off_leaving_insert"),
-  pattern = "*",
-  -- Disable paste mode upon exiting insert mode.
-  command = "set nopaste",
-})
-
-------------------------------------------------------------
--- Keep Cursor Vertically Centered
-------------------------------------------------------------
-local obs = false
-local function set_scrolloff(winid)
-  if obs then
-    -- If obs (option scrolloff balance) is true, set a smaller scrolloff.
-    vim.wo[winid].scrolloff = math.floor(math.max(10, vim.api.nvim_win_get_height(winid) / 10))
-  else
-    -- Otherwise, keep the cursor roughly centered.
-    vim.wo[winid].scrolloff = 1 + math.floor(vim.api.nvim_win_get_height(winid) / 2)
-  end
-end
-vim.api.nvim_create_autocmd({ "BufEnter", "WinEnter", "WinNew", "VimResized" }, {
-  desc = "Always keep the cursor vertically centered",
-  pattern = "*",
-  group = augroup("scrolloff_centered"),
-  callback = function()
-    set_scrolloff(0)
-  end,
-})
-
-------------------------------------------------------------
--- Auto-Reload File if Changed Externally
-------------------------------------------------------------
-vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
-  group = augroup("checktime"),
-  callback = function()
-    -- Check if the file has changed on disk and reload if necessary.
-    if vim.o.buftype ~= "nofile" then
-      vim.cmd("checktime")
-    end
-  end,
-})
-
-------------------------------------------------------------
--- Resize Splits on Vim Window Resize
-------------------------------------------------------------
-vim.api.nvim_create_autocmd({ "VimResized" }, {
-  group = augroup("resize_splits"),
-  callback = function()
-    local current_tab = vim.fn.tabpagenr()
-    -- Adjust all splits to be equal size.
-    vim.cmd("tabdo wincmd =")
-    -- Return to the original tab.
-    vim.cmd("tabnext " .. current_tab)
-  end,
-})
-
-------------------------------------------------------------
--- Close Scratch Preview Automatically
-------------------------------------------------------------
-vim.api.nvim_create_autocmd({ "CursorMovedI", "InsertLeave" }, {
-  group = augroup("close_scratch_preview"),
-  desc = "Close the popup-menu automatically",
-  pattern = "*",
-  -- Close the popup menu if it's not visible.
-  command = "if pumvisible() == 0 && !&pvw && getcmdwintype() == ''|pclose|endif",
-})
-
-------------------------------------------------------------
--- Open Files with :line Suffix at Specific Line
-------------------------------------------------------------
-vim.api.nvim_create_autocmd("BufNew", {
-  group = augroup("edit_files_with_line"),
-  desc = "Edit files with :line at the end",
-  callback = function(args)
-    local bufname = vim.api.nvim_buf_get_name(args.buf)
-    -- Match files specified with a line number, e.g., "file.txt:123"
-    local root, line = bufname:match("^(.*):(%d+)$")
-    if vim.fn.filereadable(bufname) == 0 and root and line and vim.fn.filereadable(root) == 1 then
+  ------------------------------------------------------------
+  -- Close Certain Filetypes with <q>
+  ------------------------------------------------------------
+  vim.api.nvim_create_autocmd("FileType", {
+    group = augroup("close_with_q"),
+    pattern = {
+      "checkhealth",
+      "dbout",
+      "grug-far",
+      "help",
+      "qf",
+      "startuptime",
+      "tsplayground",
+      "mininotify-history",
+      "lspinfo",
+      "lsplog",
+      "lintinfo",
+      "cmd",
+      "nvim-pack",
+    },
+    callback = function(event)
+      -- Prevent the buffer from appearing in the buffer list.
+      vim.bo[event.buf].buflisted = false
       vim.schedule(function()
-        -- Edit the root file and jump to the specified line.
-        vim.cmd.edit({ args = { root } })
-        pcall(vim.api.nvim_win_set_cursor, 0, { tonumber(line), 0 })
-        -- Remove the temporary buffer.
-        vim.api.nvim_buf_delete(args.buf, { force = true })
+        -- Map <q> in the buffer to close it and delete the buffer.
+        vim.keymap.set("n", "q", function()
+          vim.cmd("close")
+          pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+        end, {
+          buffer = event.buf,
+          silent = true,
+          desc = "Quit buffer",
+        })
       end)
+    end,
+  })
+
+  ------------------------------------------------------------
+  -- Open Buffer: Restore Last Cursor Location
+  ------------------------------------------------------------
+  vim.api.nvim_create_autocmd("BufReadPost", {
+    group = augroup("last_loc"),
+    callback = function(event)
+      local exclude = { "gitcommit" }
+      local buf = event.buf
+      -- Skip if filetype is excluded or already processed.
+      if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].last_loc then
+        return
+      end
+      vim.b[buf].last_loc = true
+      local mark = vim.api.nvim_buf_get_mark(buf, '"')
+      local lcount = vim.api.nvim_buf_line_count(buf)
+      -- Jump to last cursor position if it's within file bounds.
+      if mark[1] > 0 and mark[1] <= lcount then
+        pcall(vim.api.nvim_win_set_cursor, 0, mark)
+      end
+    end,
+  })
+
+  ------------------------------------------------------------
+  -- Remove Trailing Whitespace on Save
+  ------------------------------------------------------------
+  vim.api.nvim_create_autocmd("BufWritePre", {
+    group = augroup("remove_whitespace_on_save"),
+    pattern = "",
+    -- Remove trailing whitespace before saving.
+    command = ":%s/\\s\\+$//e",
+  })
+
+  ------------------------------------------------------------
+  -- Disable Auto-Commenting on New Lines
+  ------------------------------------------------------------
+  vim.api.nvim_create_autocmd("BufEnter", {
+    group = augroup("no_auto_commeting_new_lines"),
+    pattern = "",
+    -- Remove automatic insertion of comment leaders on new lines.
+    command = "set fo-=c fo-=r fo-=o",
+  })
+
+  ------------------------------------------------------------
+  -- Turn Off Paste Mode When Leaving Insert Mode
+  ------------------------------------------------------------
+  vim.api.nvim_create_autocmd("InsertLeave", {
+    group = augroup("paste_mode_off_leaving_insert"),
+    pattern = "*",
+    -- Disable paste mode upon exiting insert mode.
+    command = "set nopaste",
+  })
+
+  ------------------------------------------------------------
+  -- Keep Cursor Vertically Centered
+  ------------------------------------------------------------
+  local obs = false
+  local function set_scrolloff(winid)
+    if obs then
+      -- If obs (option scrolloff balance) is true, set a smaller scrolloff.
+      vim.wo[winid].scrolloff = math.floor(math.max(10, vim.api.nvim_win_get_height(winid) / 10))
+    else
+      -- Otherwise, keep the cursor roughly centered.
+      vim.wo[winid].scrolloff = 1 + math.floor(vim.api.nvim_win_get_height(winid) / 2)
     end
-  end,
-})
+  end
+  vim.api.nvim_create_autocmd({ "BufEnter", "WinEnter", "WinNew", "VimResized" }, {
+    desc = "Always keep the cursor vertically centered",
+    pattern = "*",
+    group = augroup("scrolloff_centered"),
+    callback = function()
+      set_scrolloff(0)
+    end,
+  })
 
-------------------------------------------------------------
--- Auto Toggle Cursorline Based on Window Focus
-------------------------------------------------------------
--- Show the cursorline only in the active window.
-vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
-  group = augroup("auto_cursorline_show"),
-  callback = function()
-    local win = vim.api.nvim_get_current_win()
+  ------------------------------------------------------------
+  -- Auto-Reload File if Changed Externally
+  ------------------------------------------------------------
+  vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
+    group = augroup("checktime"),
+    callback = function()
+      -- Check if the file has changed on disk and reload if necessary.
+      if vim.o.buftype ~= "nofile" then
+        vim.cmd("checktime")
+      end
+    end,
+  })
 
-    if vim.w[win].auto_cursorline then
-      vim.wo[win].cursorline = true
-      vim.w[win].auto_cursorline = nil
-    end
-  end,
-})
-vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
-  group = augroup("auto_cursorline_hide"),
-  callback = function()
-    local win = vim.api.nvim_get_current_win()
+  ------------------------------------------------------------
+  -- Resize Splits on Vim Window Resize
+  ------------------------------------------------------------
+  vim.api.nvim_create_autocmd({ "VimResized" }, {
+    group = augroup("resize_splits"),
+    callback = function()
+      local current_tab = vim.fn.tabpagenr()
+      -- Adjust all splits to be equal size.
+      vim.cmd("tabdo wincmd =")
+      -- Return to the original tab.
+      vim.cmd("tabnext " .. current_tab)
+    end,
+  })
 
-    if vim.wo[win].cursorline then
-      vim.w[win].auto_cursorline = true
-      vim.wo[win].cursorline = false
-    end
-  end,
-})
+  ------------------------------------------------------------
+  -- Close Scratch Preview Automatically
+  ------------------------------------------------------------
+  vim.api.nvim_create_autocmd({ "CursorMovedI", "InsertLeave" }, {
+    group = augroup("close_scratch_preview"),
+    desc = "Close the popup-menu automatically",
+    pattern = "*",
+    -- Close the popup menu if it's not visible.
+    command = "if pumvisible() == 0 && !&pvw && getcmdwintype() == ''|pclose|endif",
+  })
 
-------------------------------------------------------------
--- Automatically Split Help Buffers to the Right
-------------------------------------------------------------
-vim.api.nvim_create_autocmd("FileType", {
-  group = augroup("split_help_right"),
-  pattern = "help",
-  -- Move help buffers to the right side.
-  command = "wincmd L",
-})
+  ------------------------------------------------------------
+  -- Open Files with :line Suffix at Specific Line
+  ------------------------------------------------------------
+  vim.api.nvim_create_autocmd("BufNew", {
+    group = augroup("edit_files_with_line"),
+    desc = "Edit files with :line at the end",
+    callback = function(args)
+      local bufname = vim.api.nvim_buf_get_name(args.buf)
+      -- Match files specified with a line number, e.g., "file.txt:123"
+      local root, line = bufname:match("^(.*):(%d+)$")
+      if vim.fn.filereadable(bufname) == 0 and root and line and vim.fn.filereadable(root) == 1 then
+        vim.schedule(function()
+          -- Edit the root file and jump to the specified line.
+          vim.cmd.edit({ args = { root } })
+          pcall(vim.api.nvim_win_set_cursor, 0, { tonumber(line), 0 })
+          -- Remove the temporary buffer.
+          vim.api.nvim_buf_delete(args.buf, { force = true })
+        end)
+      end
+    end,
+  })
 
-------------------------------------------------------------
--- Enable treesitter
-------------------------------------------------------------
+  ------------------------------------------------------------
+  -- Auto Toggle Cursorline Based on Window Focus
+  ------------------------------------------------------------
+  -- Show the cursorline only in the active window.
+  vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
+    group = augroup("auto_cursorline_show"),
+    callback = function()
+      local win = vim.api.nvim_get_current_win()
 
-vim.api.nvim_create_autocmd("FileType", {
-  -- pattern = ensure_installed,
-  callback = function()
-    -- syntax highlighting, provided by Neovim
-    local ok = pcall(vim.treesitter.start)
+      if vim.w[win].auto_cursorline then
+        vim.wo[win].cursorline = true
+        vim.w[win].auto_cursorline = nil
+      end
+    end,
+  })
+  vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
+    group = augroup("auto_cursorline_hide"),
+    callback = function()
+      local win = vim.api.nvim_get_current_win()
 
-    if not ok then
-      return
-    end
+      if vim.wo[win].cursorline then
+        vim.w[win].auto_cursorline = true
+        vim.wo[win].cursorline = false
+      end
+    end,
+  })
 
-    -- folds, provided by Neovim
-    vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-    -- indentation, provided by nvim-treesitter
-    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-  end,
-})
+  ------------------------------------------------------------
+  -- Automatically Split Help Buffers to the Right
+  ------------------------------------------------------------
+  vim.api.nvim_create_autocmd("FileType", {
+    group = augroup("split_help_right"),
+    pattern = "help",
+    -- Move help buffers to the right side.
+    command = "wincmd L",
+  })
+
+  ------------------------------------------------------------
+  -- Enable treesitter
+  ------------------------------------------------------------
+
+  vim.api.nvim_create_autocmd("FileType", {
+    -- pattern = ensure_installed,
+    callback = function()
+      -- syntax highlighting, provided by Neovim
+      local ok = pcall(vim.treesitter.start)
+
+      if not ok then
+        return
+      end
+
+      -- folds, provided by Neovim
+      vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+      -- indentation, provided by nvim-treesitter
+      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end,
+  })
+end
+
+function M.init()
+  setup_autocmds()
+end
+
+return M
--- config/nvim/lua/mappings.lua
+++ config/nvim/lua/mappings.lua
@@ -1,83 +1,93 @@
-------------------------------------------------------------
--- Navigation: Start & End of Line
-------------------------------------------------------------
--- Move to the start of the line in normal and visual modes.
-vim.keymap.set({ "n", "v" }, "H", "^", { desc = "Move to start of line" })
--- Move to the end of the line in normal and visual modes.
-vim.keymap.set({ "n", "v" }, "L", "$", { desc = "Move to end of line" })
+local M = {}
 
-------------------------------------------------------------
--- Navigation: Better Up & Down Movement
-------------------------------------------------------------
--- Move down: Use 'gj' if no count is given, otherwise 'j'.
-vim.keymap.set({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
--- Move up: Use 'gk' if no count is given, otherwise 'k'.
-vim.keymap.set({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+local function setup_keymaps()
+  ------------------------------------------------------------
+  -- Navigation: Start & End of Line
+  ------------------------------------------------------------
+  -- Move to the start of the line in normal and visual modes.
+  vim.keymap.set({ "n", "v" }, "H", "^", { desc = "Move to start of line" })
+  -- Move to the end of the line in normal and visual modes.
+  vim.keymap.set({ "n", "v" }, "L", "$", { desc = "Move to end of line" })
 
-------------------------------------------------------------
--- Visual Mode: Indentation Adjustments
-------------------------------------------------------------
--- In visual mode, dedent and reselect the block.
-vim.keymap.set("v", "<", "<gv", { desc = "Dedent" })
--- In visual mode, indent and reselect the block.
-vim.keymap.set("v", ">", ">gv", { desc = "Indent" })
+  ------------------------------------------------------------
+  -- Navigation: Better Up & Down Movement
+  ------------------------------------------------------------
+  -- Move down: Use 'gj' if no count is given, otherwise 'j'.
+  vim.keymap.set({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+  -- Move up: Use 'gk' if no count is given, otherwise 'k'.
+  vim.keymap.set({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
 
-------------------------------------------------------------
--- Yank Behavior: Preserve Cursor Position
-------------------------------------------------------------
--- Yank text in normal and visual modes while keeping the cursor in place.
-vim.keymap.set({ "n", "x" }, "y", function()
-  -- Preserve the current cursor position when yanking.
-  local pos = vim.fn.getpos(".")
+  ------------------------------------------------------------
+  -- Visual Mode: Indentation Adjustments
+  ------------------------------------------------------------
+  -- In visual mode, dedent and reselect the block.
+  vim.keymap.set("v", "<", "<gv", { desc = "Dedent" })
+  -- In visual mode, indent and reselect the block.
+  vim.keymap.set("v", ">", ">gv", { desc = "Indent" })
 
-  vim.schedule(function()
-    vim.g.ug_ignore_cursor_moved = true
-    vim.fn.setpos(".", pos)
-  end)
-  return "y"
-end, { expr = true, noremap = true, desc = "Yank and remain cursor" })
+  ------------------------------------------------------------
+  -- Yank Behavior: Preserve Cursor Position
+  ------------------------------------------------------------
+  -- Yank text in normal and visual modes while keeping the cursor in place.
+  vim.keymap.set({ "n", "x" }, "y", function()
+    -- Preserve the current cursor position when yanking.
+    local pos = vim.fn.getpos(".")
 
-------------------------------------------------------------
--- Location & Quickfix
-------------------------------------------------------------
--- Open the location list.
-vim.keymap.set("n", "<leader>xl", "<cmd>lopen<cr>", { desc = "Location List" })
--- Open the quickfix list.
-vim.keymap.set("n", "<leader>xq", "<cmd>copen<cr>", { desc = "Quickfix List" })
+    vim.schedule(function()
+      vim.g.ug_ignore_cursor_moved = true
+      vim.fn.setpos(".", pos)
+    end)
+    return "y"
+  end, { expr = true, noremap = true, desc = "Yank and remain cursor" })
 
-------------------------------------------------------------
--- Window Splitting
-------------------------------------------------------------
--- Split window horizontally (new window appears below).
-vim.keymap.set("n", "-", "<C-W>s", { desc = "Split Window Below", remap = true })
--- Split window vertically (new window appears to the right).
-vim.keymap.set("n", "\\", "<C-W>v", { desc = "Split Window Right", remap = true })
+  ------------------------------------------------------------
+  -- Location & Quickfix
+  ------------------------------------------------------------
+  -- Open the location list.
+  vim.keymap.set("n", "<leader>xl", "<cmd>lopen<cr>", { desc = "Location List" })
+  -- Open the quickfix list.
+  vim.keymap.set("n", "<leader>xq", "<cmd>copen<cr>", { desc = "Quickfix List" })
 
-------------------------------------------------------------
--- Miscellaneous Editing
-------------------------------------------------------------
--- Select all text in the buffer.
-vim.keymap.set("n", "<C-a>", "gg<S-v>G", { desc = "Select all" })
--- Delete a character without copying it to a register.
-vim.keymap.set("n", "x", '"_x', { desc = "Delete a character without copying it" })
+  ------------------------------------------------------------
+  -- Window Splitting
+  ------------------------------------------------------------
+  -- Split window horizontally (new window appears below).
+  vim.keymap.set("n", "-", "<C-W>s", { desc = "Split Window Below", remap = true })
+  -- Split window vertically (new window appears to the right).
+  vim.keymap.set("n", "\\", "<C-W>v", { desc = "Split Window Right", remap = true })
 
-------------------------------------------------------------
--- Moving Lines in Visual Mode
-------------------------------------------------------------
--- Move selected lines down and reselect the block.
-vim.keymap.set("v", "J", ":m '>+1<cr> | :normal gv=gv<cr>", { desc = "Move line down" })
--- Move selected lines up and reselect the block.
-vim.keymap.set("v", "K", ":m '<-2<cr> | :normal gv=gv<cr>", { desc = "Move line up" })
+  ------------------------------------------------------------
+  -- Miscellaneous Editing
+  ------------------------------------------------------------
+  -- Select all text in the buffer.
+  vim.keymap.set("n", "<C-a>", "gg<S-v>G", { desc = "Select all" })
+  -- Delete a character without copying it to a register.
+  vim.keymap.set("n", "x", '"_x', { desc = "Delete a character without copying it" })
 
-------------------------------------------------------------
--- Disable Unwanted Commands
-------------------------------------------------------------
--- Map Q to no operation to avoid accidental use.
-vim.keymap.set({ "n", "x" }, "Q", "<nop>", { desc = "No op" })
+  ------------------------------------------------------------
+  -- Moving Lines in Visual Mode
+  ------------------------------------------------------------
+  -- Move selected lines down and reselect the block.
+  vim.keymap.set("v", "J", ":m '>+1<cr> | :normal gv=gv<cr>", { desc = "Move line down" })
+  -- Move selected lines up and reselect the block.
+  vim.keymap.set("v", "K", ":m '<-2<cr> | :normal gv=gv<cr>", { desc = "Move line up" })
 
-------------------------------------------------------------
--- Remove Default Neovim LSP Bindings
-------------------------------------------------------------
--- Delete default Neovim LSP bindings that are not needed.
-vim.keymap.del("s", "<C-s>") -- macos stealed the key, use `grs` instead
-vim.keymap.del("i", "<C-S>") -- macos stealed the key, use `grs` instead
+  ------------------------------------------------------------
+  -- Disable Unwanted Commands
+  ------------------------------------------------------------
+  -- Map Q to no operation to avoid accidental use.
+  vim.keymap.set({ "n", "x" }, "Q", "<nop>", { desc = "No op" })
+
+  ------------------------------------------------------------
+  -- Remove Default Neovim LSP Bindings
+  ------------------------------------------------------------
+  -- Delete default Neovim LSP bindings that are not needed.
+  vim.keymap.del("s", "<C-s>") -- macos stealed the key, use `grs` instead
+  vim.keymap.del("i", "<C-S>") -- macos stealed the key, use `grs` instead
+end
+
+function M.init()
+  setup_keymaps()
+end
+
+return M
