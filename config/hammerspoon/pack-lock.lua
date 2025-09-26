e42ae9644593b2410ae90351f8ef745d446a75fe
mtraworld <140996996+mtraworld@users.noreply.github.com>
2025-09-26 12:05:02 +0800

    fix(hammerspoon.pack): remove generatedAt from lockfile to prevent noises (#1379)

--- config/hammerspoon/pack-lock.lua
+++ config/hammerspoon/pack-lock.lua
@@ -1,8 +1,6 @@
 -- Pack lockfile - tracks exact versions of installed plugins
 -- This file should be committed to version control
--- Generated at: Fri Sep 26 11:48:32 2025
 return {
-  generatedAt = "2025-09-26T11:48:32Z",
   plugins = {
     Bindery = {
       dir = "/Users/kylewong/.hammerspoon/custom-plugins/Bindery",
