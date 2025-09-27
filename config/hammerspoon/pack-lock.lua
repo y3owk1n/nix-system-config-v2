526489c307a7e4c13ec79493288dba408ba1c047
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2025-09-27 12:17:20 +0800

    feat(hammerspoon): remove custom plugins and install them from github

--- config/hammerspoon/pack-lock.lua
+++ config/hammerspoon/pack-lock.lua
@@ -3,14 +3,16 @@
 return {
   plugins = {
     Bindery = {
-      dir = "/Users/kylewong/.hammerspoon/custom-plugins/Bindery",
       name = "Bindery",
+      resolvedCommit = "9dd885e45de7342e7fd2a20ac67c7761a9f8bf27",
       spoon = false,
+      url = "https://github.com/y3owk1n/bindery.spoon.git",
     },
     Vimnav = {
-      dir = "/Users/kylewong/.hammerspoon/custom-plugins/Vimnav",
       name = "Vimnav",
+      resolvedCommit = "aa3ef58ee5d6f492f047a49da7d45059c3f8e6f1",
       spoon = false,
+      url = "https://github.com/y3owk1n/vimnav.spoon.git",
     },
   },
   version = "1.0.0",
