fb4cf8428f47e5a3b33b18e22f19383d9a77c0c3
mtraworld <140996996+mtraworld@users.noreply.github.com>
2025-10-08 18:56:28 +0800

    feat: determinate settings & use flakehub urls instead of github

--- darwin/shared/core.nix
+++ darwin/shared/core.nix
@@ -19,6 +19,17 @@
     };
   };
 
+  # Custom settings written to /etc/nix/nix.custom.conf
+  determinate-nix.customSettings = {
+    # Enables parallel evaluation (remove this setting or set the value to 1 to disable)
+    eval-cores = 0;
+    extra-experimental-features = [
+      "build-time-fetch-tree" # Enables build-time flake inputs
+      "parallel-eval" # Enables parallel evaluation
+    ];
+    # Other settings
+  };
+
   nix.settings.trusted-users = [ username ];
 
   # enable flakes globally
