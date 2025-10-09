0fa9601b1f48c1f6e297ae405bd2976af8e46683
mtraworld <140996996+mtraworld@users.noreply.github.com>
2025-10-09 12:10:31 +0800

    feat: bump kanata to v1.10.0 (#1404)

--- home-manager/packages/kanata.nix
+++ home-manager/packages/kanata.nix
@@ -1,7 +1,25 @@
 { config, pkgs, ... }:
 {
   home.packages = with pkgs; [
-    kanata
+    # kanata
+    # NOTE: Do not delete this, just comment it out
+    # This is the right way to override a rust package after all the researches
+    (kanata.overrideAttrs (
+      finalAttrs: prevAttrs: {
+        cargoHash = "sha256-U6qBHWp4Hh2XRH9XR32CYeGbuKTIaEk2NDCQU5obMGc=";
+        src = fetchFromGitHub {
+          owner = "jtroo";
+          repo = "kanata";
+          rev = "v1.10.0-prerelease-1";
+          sha256 = "sha256-8CpHz7bHHhzsR8bLu0GGH+vzHGZvdosrKpHepo9iHDM=";
+        };
+        version = "1.10.0-prerelease-1";
+        cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
+          inherit (finalAttrs) pname src version;
+          hash = finalAttrs.cargoHash;
+        };
+      }
+    ))
   ];
 
   xdg.configFile.kanata = {
