2d9d3ee59ebffbf6e360998ab2bb480386fbad79
mtraworld <140996996+mtraworld@users.noreply.github.com>
2025-10-23 17:13:31 +0800

    fix: centralise all overrides into overlays instead of inline (#1434)

--- darwin/shared/overlays.nix
+++ darwin/shared/overlays.nix
@@ -1,5 +1,6 @@
 {
   nixos-npm-ls,
+  pkgs,
   ...
 }:
 {
@@ -8,7 +9,7 @@
       # custom derivations
       hammerspoon = final.callPackage ../overlays/hammerspoon.nix { };
 
-      # overrides
+      # pagkage overrides
       aerospace = prev.aerospace.overrideAttrs (o: rec {
         version = "0.19.2-Beta";
         src = prev.fetchzip {
@@ -16,6 +17,34 @@
           sha256 = "sha256-6RyGw84GhGwULzN0ObjsB3nzRu1HYQS/qoCvzVWOYWQ=";
         };
       });
+      kanata = prev.kanata.overrideAttrs (
+        finalAttrs: prevAttrs: {
+          cargoHash = "sha256-pEA1i7abdfBjAHwSAjwO4RKlmTMHgeDLBbbfzMbB2xg=";
+          src = prev.fetchFromGitHub {
+            owner = "jtroo";
+            repo = "kanata";
+            rev = "v1.10.0-prerelease-2";
+            sha256 = "sha256-aQDeMfkb6wjwQ40wP0XE2JcaOrHArvItVfB6QsmVpuc=";
+          };
+          version = "1.10.0-prerelease-2";
+          cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
+            inherit (finalAttrs) pname src version;
+            hash = finalAttrs.cargoHash;
+          };
+        }
+      );
+
+      # tmux plugins overrides
+      tmuxPlugins = prev.tmuxPlugins // {
+        vim-tmux-navigator = prev.tmuxPlugins.vim-tmux-navigator.overrideAttrs (_: {
+          src = prev.fetchFromGitHub {
+            owner = "christoomey";
+            repo = "vim-tmux-navigator";
+            rev = "c45243dc1f32ac6bcf6068e5300f3b2b237e576a";
+            hash = "sha256-IEPnr/GdsAnHzdTjFnXCuMyoNLm3/Jz4cBAM0AJBrj8=";
+          };
+        });
+      };
     })
   ]
   ++ nixos-npm-ls.overlays;
