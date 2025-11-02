27f9961e6d45d4b8d4bc4768c44d3be622170da9
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2025-11-02 13:00:58 +0800

    feat: bump govim to v1.1.0 (#1455)

--- darwin/overlays/govim.nix
+++ darwin/overlays/govim.nix
@@ -9,13 +9,13 @@
 }:
 buildGoModule (finalAttrs: {
   pname = "govim";
-  version = "1.0.3";
+  version = "1.1.0";
 
   src = fetchFromGitHub {
     owner = "y3owk1n";
     repo = "govim";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-mUHFiEsQQjiclkh6QpzQlXQJggaAJqCpYjODyHy3LI0=";
+    hash = "sha256-UN0Y49X8fD+2gSJrnv8qGzthKPrpBv51sxA7BEUViWc=";
   };
 
   vendorHash = "sha256-x5NB18fP8ERIB5qeMAMyMnSoDEF2+g+NoJKrC+kIj+k=";
