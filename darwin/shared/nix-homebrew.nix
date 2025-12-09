a98c6cbcfd173727db99f1186486a582dfdce127
mtraworld <140996996+mtraworld@users.noreply.github.com>
2025-12-09 15:06:32 +0800

    fix: clean homebrew (#1495)

--- darwin/shared/nix-homebrew.nix
+++ darwin/shared/nix-homebrew.nix
@@ -8,27 +8,15 @@
 }:
 {
   nix-homebrew = {
-    # Install Homebrew under the default prefix
     enable = true;
-
-    # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
     enableRosetta = true;
-
-    # User owning the Homebrew prefix
     user = "${username}";
-
-    # Optional: Declarative tap management
     taps = {
       "homebrew/homebrew-core" = homebrew-core;
       "homebrew/homebrew-cask" = homebrew-cask;
       "homebrew/homebrew-bundle" = homebrew-bundle;
       "y3owk1n/tap" = homebrew-y3owk1n;
     };
-
-    # Optional: Enable fully-declarative tap management
-    #
-    # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
-    mutableTaps = true;
-    autoMigrate = true;
+    mutableTaps = false;
   };
 }
