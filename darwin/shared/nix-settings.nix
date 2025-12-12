a21cc9b033bc09cbd455dd2b4d111418d8800a0f
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2025-12-12 23:39:38 +0800

    fix: move `extra-substituters` & `extra-trusted-public-keys` to determinate nix settings (#1503)

--- darwin/shared/nix-settings.nix
+++ darwin/shared/nix-settings.nix
@@ -11,6 +11,16 @@
       "root"
       username
     ];
+    trusted-substituters = [
+      "https://cache.nixos.org"
+      "https://cache.flakehub.com"
+      "https://install.determinate.systems"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
+      "cache.determinate.systems:aHRYSxYP2rxdNsFfU5Wd0Q8d8Qqjrx4H8YB0uHK7P68="
+    ];
     # Enables parallel evaluation (remove this setting or set the value to 1 to disable)
     eval-cores = 0;
     extra-experimental-features = [
