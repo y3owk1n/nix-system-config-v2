2e4380474c07d8d01155587b478d41c214c462fc
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2025-12-17 23:02:16 +0800

    feat: update `lsp` keymaps to latest `:lsp`

--- config/nvim/lua/plugins/_nvim-lspconfig.lua
+++ config/nvim/lua/plugins/_nvim-lspconfig.lua
@@ -19,11 +19,16 @@ function M.setup()
   end
 
   vim.keymap.set("n", "<leader>l", "", { desc = "lsp" })
-  vim.keymap.set("n", "<leader>li", "<cmd>LspInfo<cr>", { desc = "lsp info" })
-  vim.keymap.set("n", "<leader>lr", "<cmd>LspRestart<cr>", { desc = "lsp restart" })
-  vim.keymap.set("n", "<leader>ls", "<cmd>LspStart<cr>", { desc = "lsp start" })
-  vim.keymap.set("n", "<leader>ld", "<cmd>LspStop<cr>", { desc = "lsp stop" })
-  vim.keymap.set("n", "<leader>ll", "<cmd>LspLog<cr>", { desc = "lsp log" })
+  vim.keymap.set("n", "<leader>li", "<cmd>checkhealth vim.lsp<cr>", { desc = "lsp info" })
+  vim.keymap.set("n", "<leader>lr", "<cmd>lsp restart<cr>", { desc = "lsp restart" })
+  vim.keymap.set("n", "<leader>ls", "<cmd>lsp enable<cr>", { desc = "lsp start" })
+  vim.keymap.set("n", "<leader>ld", "<cmd>lsp disable<cr>", { desc = "lsp stop" })
+  vim.keymap.set(
+    "n",
+    "<leader>ll",
+    "<cmd>lua vim.cmd('tabnew ' .. vim.lsp.log.get_filename())<cr>",
+    { desc = "lsp log" }
+  )
 
   vim.schedule(function()
     require("lsp.init").setup({
