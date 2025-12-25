2999fc2c2d6cd3128eb4f9182dd05e9bfa425e9a
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2025-12-25 19:10:43 +0800

    feat: make `nvs` works with nixos by using `nix-ld`

--- parts/overlays.nix
+++ parts/overlays.nix
@@ -12,7 +12,6 @@ in
     inputs.neru.overlays.default
     inputs.nvs.overlays.default
     inputs.nixos-npm-ls.overlays.default
-    inputs.neovim-nightly-overlay.overlays.default
 
     # Custom overlays
     inputs.self.overlays.custom
