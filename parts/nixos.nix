dd1d55db046133db8b1650f93bae1efa9c6c380c
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2025-12-27 11:12:50 +0800

    feat: add nixos-vb

--- parts/nixos.nix
+++ parts/nixos.nix
@@ -7,5 +7,6 @@
   flake.nixosConfigurations = {
     "nixos-orb" = import ./hosts/nixos-orb.nix { inherit inputs; };
     "nixos-vmware-fusion" = import ./hosts/nixos-vmware-fusion.nix { inherit inputs; };
+    "nixos-vb" = import ./hosts/nixos-vb.nix { inherit inputs; };
   };
 }
