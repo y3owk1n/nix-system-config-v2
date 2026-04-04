37440fb20fbb1f2ab4a17aa2b6fc39c65b59643f
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2026-04-04 10:24:36 +0800

    feat(yabai): bump to 7.1.18

--- parts/overlays/overrides.nix
+++ parts/overlays/overrides.nix
@@ -15,10 +15,10 @@ _: {
 
     # Override yabai to use the specific version
     yabai = prev.yabai.overrideAttrs (_: rec {
-      version = "7.1.17";
+      version = "7.1.18";
       src = fetchTarball {
         url = "https://github.com/koekeishiya/yabai/releases/download/v${version}/yabai-v${version}.tar.gz";
-        sha256 = "sha256:0d2mhfsd0s7d0233qib28rl8jzqk90hjllf8735k162dqdh7ki1c";
+        sha256 = "sha256:09g9rbf4mhfw4baglnz209c4j7ww8z88k29cvzz3c0xy0dn140qc";
       };
     });
 
