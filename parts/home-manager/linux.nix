da45246e246c263132f9e62fd913a8075afc9f19
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2026-04-14 11:18:11 +0800

    refactor(fedora): use flake parts

--- parts/home-manager/linux.nix
+++ parts/home-manager/linux.nix
@@ -1,26 +1,6 @@
 { inputs, ... }:
 {
   flake.homeConfigurations = {
-    "kylewong-fedora" = inputs.home-manager.lib.homeManagerConfiguration {
-      pkgs = inputs.nixpkgs.legacyPackages.aarch64-linux;
-      extraSpecialArgs = { inherit inputs; };
-      modules = [
-        {
-          nixpkgs.overlays = [ inputs.self.overlays.default ];
-          stylix = {
-            enable = true;
-            base16Scheme = ../../config/pastel-twilight/base16.yml;
-          };
-        }
-        ../../home-manager/shared/base.nix
-        ../../home-manager/hosts/nixos-fedora.nix
-        # nvs
-        inputs.nvs.homeManagerModules.default
-        # neru
-        inputs.neru.homeManagerModules.default
-        # stylix
-        inputs.stylix.homeModules.stylix
-      ];
-    };
+    "fedora" = import ../hosts/fedora.nix { inherit inputs; };
   };
 }
---
+++ parts/hosts/fedora.nix
@@ -0,0 +1,53 @@
+{ inputs, ... }:
+let
+  username = "kylewong";
+  useremail = "62775956+y3owk1n@users.noreply.github.com"; # only used for git
+  hostname = "fedora";
+  githubuser = "y3owk1n";
+  githubname = "Kyle Wong";
+  gpgkeyid = "F3EBDBB90E035E02";
+in
+inputs.home-manager.lib.homeManagerConfiguration {
+  pkgs = import inputs.nixpkgs {
+    system = "aarch64-linux";
+    config.allowUnfree = true;
+  };
+  extraSpecialArgs = (builtins.removeAttrs inputs [ "self" ]) // {
+    inherit
+      username
+      useremail
+      hostname
+      githubuser
+      githubname
+      gpgkeyid
+      ;
+  };
+  modules = [
+    inputs.self.homeModules.shared
+    {
+      inherit
+        username
+        useremail
+        hostname
+        githubuser
+        githubname
+        gpgkeyid
+        ;
+    }
+    {
+      nixpkgs.overlays = [ inputs.self.overlays.default ];
+      stylix = {
+        enable = true;
+        base16Scheme = ../../config/pastel-twilight/base16.yml;
+      };
+    }
+    ../../home-manager/shared/base.nix
+    ../../home-manager/hosts/fedora.nix
+    # nvs
+    inputs.nvs.homeManagerModules.default
+    # neru
+    inputs.neru.homeManagerModules.default
+    # stylix
+    inputs.stylix.homeModules.stylix
+  ];
+}
