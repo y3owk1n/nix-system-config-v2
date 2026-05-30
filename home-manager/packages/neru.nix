40081071ad3dc8d39620949e9ad0d4a0b354e4f6
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2026-05-30 13:28:33 +0800

    feat: use skhd for yabai commands
    so that i can still use the os like a human when disabling neru for dev

--- home-manager/packages/neru.nix
+++ home-manager/packages/neru.nix
@@ -24,65 +24,65 @@
       "Ctrl+F" = "recursive_grid --cursor-selection-mode hold"
       "Ctrl+S" = "scroll"
 
-      # These keys dont have to be here, i didn't want to install another program to run these
-      "Cmd+Alt+Shift+Ctrl+F" = "exec open -a \"Finder\""
-      # "Cmd+Alt+Shift+Ctrl+B" = "exec open -a \"Brave Browser\""
-      # "Cmd+Alt+Shift+Ctrl+B" = "exec open -a \"Helium\""
-      "Cmd+Alt+Shift+Ctrl+B" = "exec open -a \"Safari\""
-      "Cmd+Alt+Shift+Ctrl+T" = "exec open -a \"Ghostty\""
-      "Cmd+Alt+Shift+Ctrl+N" = "exec open -a \"Notes\""
-      "Cmd+Alt+Shift+Ctrl+R" = "exec open -a \"Reminders\""
-      "Cmd+Alt+Shift+Ctrl+M" = "exec open -a \"Mail\""
-      "Cmd+Alt+Shift+Ctrl+C" = "exec open -a \"Calendar\""
-      "Cmd+Alt+Shift+Ctrl+W" = "exec open -a \"WhatsApp\""
-      "Cmd+Alt+Shift+Ctrl+P" = "exec open -a \"Passwords\""
-      "Cmd+Alt+Shift+Ctrl+S" = "exec open -a \"System Settings\""
-      "Cmd+Alt+Shift+Ctrl+A" = "exec open -a \"Activity Monitor\""
-
-      # If yabai is uninstalled or not working anymore, set these keys in system settings and bear with the animations
-      "Cmd+Alt+Shift+Ctrl+1" = "exec yabai -m space --focus 1"
-      "Cmd+Alt+Shift+Ctrl+2" = "exec yabai -m space --focus 2"
-      "Cmd+Alt+Shift+Ctrl+3" = "exec yabai -m space --focus 3"
-      "Cmd+Alt+Shift+Ctrl+4" = "exec yabai -m space --focus 4"
-      "Cmd+Alt+Shift+Ctrl+5" = "exec yabai -m space --focus 5"
-      "Cmd+Alt+Shift+Ctrl+6" = "exec yabai -m space --focus 6"
-      "Cmd+Alt+Shift+Ctrl+7" = "exec yabai -m space --focus 7"
-      "Cmd+Alt+Shift+Ctrl+8" = "exec yabai -m space --focus 8"
-      "Cmd+Alt+Shift+Ctrl+9" = "exec yabai -m space --focus 9"
-
-      "Alt+Shift+1" = ["exec yabai -m window --space 1", "exec yabai -m space --focus 1"]
-      "Alt+Shift+2" = ["exec yabai -m window --space 2", "exec yabai -m space --focus 2"]
-      "Alt+Shift+3" = ["exec yabai -m window --space 3", "exec yabai -m space --focus 3"]
-      "Alt+Shift+4" = ["exec yabai -m window --space 4", "exec yabai -m space --focus 4"]
-      "Alt+Shift+5" = ["exec yabai -m window --space 5", "exec yabai -m space --focus 5"]
-      "Alt+Shift+6" = ["exec yabai -m window --space 6", "exec yabai -m space --focus 6"]
-      "Alt+Shift+7" = ["exec yabai -m window --space 7", "exec yabai -m space --focus 7"]
-      "Alt+Shift+8" = ["exec yabai -m window --space 8", "exec yabai -m space --focus 8"]
-      "Alt+Shift+9" = ["exec yabai -m window --space 9", "exec yabai -m space --focus 9"]
-
-      "Alt+H" = "exec yabai -m window --focus west"
-      "Alt+J" = "exec yabai -m window --focus south"
-      "Alt+K" = "exec yabai -m window --focus north"
-      "Alt+L" = "exec yabai -m window --focus east"
-
-      "Alt+Shift+H" = "exec yabai -m window --swap west"
-      "Alt+Shift+J" = "exec yabai -m window --swap south"
-      "Alt+Shift+K" = "exec yabai -m window --swap north"
-      "Alt+Shift+L" = "exec yabai -m window --swap east"
-
-      "Alt+M" = "exec yabai -m window --toggle zoom-fullscreen"
-      "Alt+F" = "exec yabai -m window --toggle float; yabai -m window --grid 4:4:1:1:2:2"
-
-      # Fallback to move window to spaces without yabai
-      # "Alt+Shift+1" = ["action save_cursor_pos", "action move_mouse --window --y -1000 --x -1000", "action sleep 0.05", "action move_mouse_relative --dx 100 --dy 2", "action sleep 0.05", "action mouse_down", "action sleep 0.1", "action move_mouse_relative --dx 5 --dy 5", "action sleep 0.1", "action feed cmd+shift+ctrl+alt+1", "action sleep 0.2", "action mouse_up", "action sleep 0.05", "exec yabai -m space --balance", "action restore_cursor_pos"]
-      # "Alt+Shift+2" = ["action save_cursor_pos", "action move_mouse --window --y -1000 --x -1000", "action sleep 0.05", "action move_mouse_relative --dx 100 --dy 2", "action sleep 0.05", "action mouse_down", "action sleep 0.1", "action move_mouse_relative --dx 5 --dy 5", "action sleep 0.1", "action feed cmd+shift+ctrl+alt+2", "action sleep 0.2", "action mouse_up", "action sleep 0.05", "exec yabai -m space --balance", "action restore_cursor_pos"]
-      # "Alt+Shift+3" = ["action save_cursor_pos", "action move_mouse --window --y -1000 --x -1000", "action sleep 0.05", "action move_mouse_relative --dx 100 --dy 2", "action sleep 0.05", "action mouse_down", "action sleep 0.1", "action move_mouse_relative --dx 5 --dy 5", "action sleep 0.1", "action feed cmd+shift+ctrl+alt+3", "action sleep 0.2", "action mouse_up", "action sleep 0.05", "exec yabai -m space --balance", "action restore_cursor_pos"]
-      # "Alt+Shift+4" = ["action save_cursor_pos", "action move_mouse --window --y -1000 --x -1000", "action sleep 0.05", "action move_mouse_relative --dx 100 --dy 2", "action sleep 0.05", "action mouse_down", "action sleep 0.1", "action move_mouse_relative --dx 5 --dy 5", "action sleep 0.1", "action feed cmd+shift+ctrl+alt+4", "action sleep 0.2", "action mouse_up", "action sleep 0.05", "exec yabai -m space --balance", "action restore_cursor_pos"]
-      # "Alt+Shift+5" = ["action save_cursor_pos", "action move_mouse --window --y -1000 --x -1000", "action sleep 0.05", "action move_mouse_relative --dx 100 --dy 2", "action sleep 0.05", "action mouse_down", "action sleep 0.1", "action move_mouse_relative --dx 5 --dy 5", "action sleep 0.1", "action feed cmd+shift+ctrl+alt+5", "action sleep 0.2", "action mouse_up", "action sleep 0.05", "exec yabai -m space --balance", "action restore_cursor_pos"]
-      # "Alt+Shift+6" = ["action save_cursor_pos", "action move_mouse --window --y -1000 --x -1000", "action sleep 0.05", "action move_mouse_relative --dx 100 --dy 2", "action sleep 0.05", "action mouse_down", "action sleep 0.1", "action move_mouse_relative --dx 5 --dy 5", "action sleep 0.1", "action feed cmd+shift+ctrl+alt+6", "action sleep 0.2", "action mouse_up", "action sleep 0.05", "exec yabai -m space --balance", "action restore_cursor_pos"]
-      # "Alt+Shift+7" = ["action save_cursor_pos", "action move_mouse --window --y -1000 --x -1000", "action sleep 0.05", "action move_mouse_relative --dx 100 --dy 2", "action sleep 0.05", "action mouse_down", "action sleep 0.1", "action move_mouse_relative --dx 5 --dy 5", "action sleep 0.1", "action feed cmd+shift+ctrl+alt+7", "action sleep 0.2", "action mouse_up", "action sleep 0.05", "exec yabai -m space --balance", "action restore_cursor_pos"]
-      # "Alt+Shift+8" = ["action save_cursor_pos", "action move_mouse --window --y -1000 --x -1000", "action sleep 0.05", "action move_mouse_relative --dx 100 --dy 2", "action sleep 0.05", "action mouse_down", "action sleep 0.1", "action move_mouse_relative --dx 5 --dy 5", "action sleep 0.1", "action feed cmd+shift+ctrl+alt+8", "action sleep 0.2", "action mouse_up", "action sleep 0.05", "exec yabai -m space --balance", "action restore_cursor_pos"]
-      # "Alt+Shift+9" = ["action save_cursor_pos", "action move_mouse --window --y -1000 --x -1000", "action sleep 0.05", "action move_mouse_relative --dx 100 --dy 2", "action sleep 0.05", "action mouse_down", "action sleep 0.1", "action move_mouse_relative --dx 5 --dy 5", "action sleep 0.1", "action feed cmd+shift+ctrl+alt+9", "action sleep 0.2", "action mouse_up", "action sleep 0.05", "exec yabai -m space --balance", "action restore_cursor_pos"]
+      # # These keys dont have to be here, i didn't want to install another program to run these
+      # "Cmd+Alt+Shift+Ctrl+F" = "exec open -a \"Finder\""
+      # # "Cmd+Alt+Shift+Ctrl+B" = "exec open -a \"Brave Browser\""
+      # # "Cmd+Alt+Shift+Ctrl+B" = "exec open -a \"Helium\""
+      # "Cmd+Alt+Shift+Ctrl+B" = "exec open -a \"Safari\""
+      # "Cmd+Alt+Shift+Ctrl+T" = "exec open -a \"Ghostty\""
+      # "Cmd+Alt+Shift+Ctrl+N" = "exec open -a \"Notes\""
+      # "Cmd+Alt+Shift+Ctrl+R" = "exec open -a \"Reminders\""
+      # "Cmd+Alt+Shift+Ctrl+M" = "exec open -a \"Mail\""
+      # "Cmd+Alt+Shift+Ctrl+C" = "exec open -a \"Calendar\""
+      # "Cmd+Alt+Shift+Ctrl+W" = "exec open -a \"WhatsApp\""
+      # "Cmd+Alt+Shift+Ctrl+P" = "exec open -a \"Passwords\""
+      # "Cmd+Alt+Shift+Ctrl+S" = "exec open -a \"System Settings\""
+      # "Cmd+Alt+Shift+Ctrl+A" = "exec open -a \"Activity Monitor\""
+      #
+      # # If yabai is uninstalled or not working anymore, set these keys in system settings and bear with the animations
+      # "Cmd+Alt+Shift+Ctrl+1" = "exec yabai -m space --focus 1"
+      # "Cmd+Alt+Shift+Ctrl+2" = "exec yabai -m space --focus 2"
+      # "Cmd+Alt+Shift+Ctrl+3" = "exec yabai -m space --focus 3"
+      # "Cmd+Alt+Shift+Ctrl+4" = "exec yabai -m space --focus 4"
+      # "Cmd+Alt+Shift+Ctrl+5" = "exec yabai -m space --focus 5"
+      # "Cmd+Alt+Shift+Ctrl+6" = "exec yabai -m space --focus 6"
+      # "Cmd+Alt+Shift+Ctrl+7" = "exec yabai -m space --focus 7"
+      # "Cmd+Alt+Shift+Ctrl+8" = "exec yabai -m space --focus 8"
+      # "Cmd+Alt+Shift+Ctrl+9" = "exec yabai -m space --focus 9"
+      #
+      # "Alt+Shift+1" = ["exec yabai -m window --space 1", "exec yabai -m space --focus 1"]
+      # "Alt+Shift+2" = ["exec yabai -m window --space 2", "exec yabai -m space --focus 2"]
+      # "Alt+Shift+3" = ["exec yabai -m window --space 3", "exec yabai -m space --focus 3"]
+      # "Alt+Shift+4" = ["exec yabai -m window --space 4", "exec yabai -m space --focus 4"]
+      # "Alt+Shift+5" = ["exec yabai -m window --space 5", "exec yabai -m space --focus 5"]
+      # "Alt+Shift+6" = ["exec yabai -m window --space 6", "exec yabai -m space --focus 6"]
+      # "Alt+Shift+7" = ["exec yabai -m window --space 7", "exec yabai -m space --focus 7"]
+      # "Alt+Shift+8" = ["exec yabai -m window --space 8", "exec yabai -m space --focus 8"]
+      # "Alt+Shift+9" = ["exec yabai -m window --space 9", "exec yabai -m space --focus 9"]
+      #
+      # "Alt+H" = "exec yabai -m window --focus west"
+      # "Alt+J" = "exec yabai -m window --focus south"
+      # "Alt+K" = "exec yabai -m window --focus north"
+      # "Alt+L" = "exec yabai -m window --focus east"
+      #
+      # "Alt+Shift+H" = "exec yabai -m window --swap west"
+      # "Alt+Shift+J" = "exec yabai -m window --swap south"
+      # "Alt+Shift+K" = "exec yabai -m window --swap north"
+      # "Alt+Shift+L" = "exec yabai -m window --swap east"
+      #
+      # "Alt+M" = "exec yabai -m window --toggle zoom-fullscreen"
+      # "Alt+F" = "exec yabai -m window --toggle float; yabai -m window --grid 4:4:1:1:2:2"
+      #
+      # # Fallback to move window to spaces without yabai
+      # # "Alt+Shift+1" = ["action save_cursor_pos", "action move_mouse --window --y -1000 --x -1000", "action sleep 0.05", "action move_mouse_relative --dx 100 --dy 2", "action sleep 0.05", "action mouse_down", "action sleep 0.1", "action move_mouse_relative --dx 5 --dy 5", "action sleep 0.1", "action feed cmd+shift+ctrl+alt+1", "action sleep 0.2", "action mouse_up", "action sleep 0.05", "exec yabai -m space --balance", "action restore_cursor_pos"]
+      # # "Alt+Shift+2" = ["action save_cursor_pos", "action move_mouse --window --y -1000 --x -1000", "action sleep 0.05", "action move_mouse_relative --dx 100 --dy 2", "action sleep 0.05", "action mouse_down", "action sleep 0.1", "action move_mouse_relative --dx 5 --dy 5", "action sleep 0.1", "action feed cmd+shift+ctrl+alt+2", "action sleep 0.2", "action mouse_up", "action sleep 0.05", "exec yabai -m space --balance", "action restore_cursor_pos"]
+      # # "Alt+Shift+3" = ["action save_cursor_pos", "action move_mouse --window --y -1000 --x -1000", "action sleep 0.05", "action move_mouse_relative --dx 100 --dy 2", "action sleep 0.05", "action mouse_down", "action sleep 0.1", "action move_mouse_relative --dx 5 --dy 5", "action sleep 0.1", "action feed cmd+shift+ctrl+alt+3", "action sleep 0.2", "action mouse_up", "action sleep 0.05", "exec yabai -m space --balance", "action restore_cursor_pos"]
+      # # "Alt+Shift+4" = ["action save_cursor_pos", "action move_mouse --window --y -1000 --x -1000", "action sleep 0.05", "action move_mouse_relative --dx 100 --dy 2", "action sleep 0.05", "action mouse_down", "action sleep 0.1", "action move_mouse_relative --dx 5 --dy 5", "action sleep 0.1", "action feed cmd+shift+ctrl+alt+4", "action sleep 0.2", "action mouse_up", "action sleep 0.05", "exec yabai -m space --balance", "action restore_cursor_pos"]
+      # # "Alt+Shift+5" = ["action save_cursor_pos", "action move_mouse --window --y -1000 --x -1000", "action sleep 0.05", "action move_mouse_relative --dx 100 --dy 2", "action sleep 0.05", "action mouse_down", "action sleep 0.1", "action move_mouse_relative --dx 5 --dy 5", "action sleep 0.1", "action feed cmd+shift+ctrl+alt+5", "action sleep 0.2", "action mouse_up", "action sleep 0.05", "exec yabai -m space --balance", "action restore_cursor_pos"]
+      # # "Alt+Shift+6" = ["action save_cursor_pos", "action move_mouse --window --y -1000 --x -1000", "action sleep 0.05", "action move_mouse_relative --dx 100 --dy 2", "action sleep 0.05", "action mouse_down", "action sleep 0.1", "action move_mouse_relative --dx 5 --dy 5", "action sleep 0.1", "action feed cmd+shift+ctrl+alt+6", "action sleep 0.2", "action mouse_up", "action sleep 0.05", "exec yabai -m space --balance", "action restore_cursor_pos"]
+      # # "Alt+Shift+7" = ["action save_cursor_pos", "action move_mouse --window --y -1000 --x -1000", "action sleep 0.05", "action move_mouse_relative --dx 100 --dy 2", "action sleep 0.05", "action mouse_down", "action sleep 0.1", "action move_mouse_relative --dx 5 --dy 5", "action sleep 0.1", "action feed cmd+shift+ctrl+alt+7", "action sleep 0.2", "action mouse_up", "action sleep 0.05", "exec yabai -m space --balance", "action restore_cursor_pos"]
+      # # "Alt+Shift+8" = ["action save_cursor_pos", "action move_mouse --window --y -1000 --x -1000", "action sleep 0.05", "action move_mouse_relative --dx 100 --dy 2", "action sleep 0.05", "action mouse_down", "action sleep 0.1", "action move_mouse_relative --dx 5 --dy 5", "action sleep 0.1", "action feed cmd+shift+ctrl+alt+8", "action sleep 0.2", "action mouse_up", "action sleep 0.05", "exec yabai -m space --balance", "action restore_cursor_pos"]
+      # # "Alt+Shift+9" = ["action save_cursor_pos", "action move_mouse --window --y -1000 --x -1000", "action sleep 0.05", "action move_mouse_relative --dx 100 --dy 2", "action sleep 0.05", "action mouse_down", "action sleep 0.1", "action move_mouse_relative --dx 5 --dy 5", "action sleep 0.1", "action feed cmd+shift+ctrl+alt+9", "action sleep 0.2", "action mouse_up", "action sleep 0.05", "exec yabai -m space --balance", "action restore_cursor_pos"]
 
       # ============================================================================
       # Hints
--- home-manager/packages/skhd.nix
+++ home-manager/packages/skhd.nix
@@ -2,22 +2,35 @@
 let
   skhd = pkgs.stdenv.mkDerivation rec {
     pname = "skhd-zig";
-    version = "0.0.15";
+    version = "0.1.7";
 
-    src = pkgs.fetchzip {
+    src = pkgs.fetchurl {
       url = "https://github.com/jackielii/skhd.zig/releases/download/v${version}/skhd-arm64-macos.tar.gz";
-      sha256 = "sha256-6ffSUWdEhtSnZOxb8lYxtHAoJaPIbojl/C1C19J4BIU=";
-      stripRoot = false;
+      sha256 = "sha256-pk1/rj20tQ1BFwCln7SCa42WVxlOw5NY1hrinvkDlWE=";
     };
 
-    phases = [ "installPhase" ];
+    nativeBuildInputs = [
+      pkgs.installShellFiles
+    ];
+
+    phases = [
+      "unpackPhase"
+      "installPhase"
+    ];
+
+    unpackPhase = ''
+      tar -xvf $src
+    '';
 
     dontBuild = true; # nothing to compile
 
     installPhase = ''
+      mkdir -p $out/Applications
+      mv skhd.app $out/Applications
+
       mkdir -p $out/bin
-      cp $src/skhd-arm64-macos $out/bin/skhd
-      chmod +x $out/bin/skhd
+      ln -s $out/Applications/skhd.app/Contents/MacOS/skhd $out/bin/skhd
+      ln -s $out/Applications/skhd.app/Contents/MacOS/skhd-grabber $out/bin/skhd-grabber
     '';
 
     meta = {
@@ -27,7 +40,7 @@ let
 in
 {
   services.skhd = {
-    enable = false;
+    enable = true;
     package = skhd;
     config = ''
       .shell "/bin/dash"
@@ -36,15 +49,49 @@ in
 
       # launchers
       hyper - f : @open("finder")
-      hyper - b : @open("firefox")
+      hyper - b : @open("safari")
       hyper - t : @open("ghostty")
       hyper - n : @open("notes")
+      hyper - r : @open("reminders")
       hyper - m : @open("mail")
       hyper - c : @open("calendar")
       hyper - w : @open("WhatsApp")
-      hyper - s : @open("System Settings")
       hyper - p : @open("Passwords")
+      hyper - s : @open("System Settings")
       hyper - a : @open("Activity Monitor")
+
+      hyper - 1 : yabai -m space --focus 1
+      hyper - 2 : yabai -m space --focus 2
+      hyper - 3 : yabai -m space --focus 3
+      hyper - 4 : yabai -m space --focus 4
+      hyper - 5 : yabai -m space --focus 5
+      hyper - 6 : yabai -m space --focus 6
+      hyper - 7 : yabai -m space --focus 7
+      hyper - 8 : yabai -m space --focus 8
+      hyper - 9 : yabai -m space --focus 9
+
+      alt + shift - 1 : yabai -m window --space 1; yabai -m space --focus 1
+      alt + shift - 2 : yabai -m window --space 2; yabai -m space --focus 2
+      alt + shift - 3 : yabai -m window --space 3; yabai -m space --focus 3
+      alt + shift - 4 : yabai -m window --space 4; yabai -m space --focus 4
+      alt + shift - 5 : yabai -m window --space 5; yabai -m space --focus 5
+      alt + shift - 6 : yabai -m window --space 6; yabai -m space --focus 6
+      alt + shift - 7 : yabai -m window --space 7; yabai -m space --focus 7
+      alt + shift - 8 : yabai -m window --space 8; yabai -m space --focus 8
+      alt + shift - 9 : yabai -m window --space 9; yabai -m space --focus 9
+
+      alt - h : yabai -m window --focus west
+      alt - j : yabai -m window --focus south
+      alt - k : yabai -m window --focus north
+      alt - l : yabai -m window --focus east
+
+      alt + shift - h : yabai -m window --swap west
+      alt + shift - j : yabai -m window --swap south
+      alt + shift - k : yabai -m window --swap north
+      alt + shift - l : yabai -m window --swap east
+
+      alt - m : yabai -m window --toggle zoom-fullscreen
+      alt - f : yabai -m window --toggle float
     '';
   };
 }
