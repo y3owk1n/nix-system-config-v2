8f88484c48727650e705af0eff20498856d853ce
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2026-07-03 16:33:12 +0800

    feat(nvs): hook theming with stylix colors

--- modules/home/packages/nvs.nix
+++ modules/home/packages/nvs.nix
@@ -1,20 +1,22 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   programs.nvs = {
     enable = true;
     # package = pkgs.nvs;
     package = pkgs.nvs-source;
-    # package = pkgs.nvs-source.overrideAttrs (_: {
-    #   postPatch = ''
-    #     substituteInPlace go.mod \
-    #       --replace-fail "go 1.26.0" "go 1.25.5"
-    #
-    #     # Verify it worked
-    #     echo "=== go.mod after patch ==="
-    #     grep "^go " go.mod || true
-    #   '';
-    # });
     enableAutoSwitch = false;
     useGlobalCache = true;
+  };
+
+  home.sessionVariables = {
+    NVS_COLOR_PRIMARY = "#${config.lib.stylix.colors.base0E}";
+    NVS_COLOR_TEXT = "#${config.lib.stylix.colors.base05}";
+    NVS_COLOR_MUTED = "#${config.lib.stylix.colors.base04}";
+    NVS_COLOR_SUBTLE = "#${config.lib.stylix.colors.base03}";
+    NVS_COLOR_BORDER = "#${config.lib.stylix.colors.base02}";
+    NVS_COLOR_ACCENT = "#${config.lib.stylix.colors.base0D}";
+    NVS_COLOR_SUCCESS = "#${config.lib.stylix.colors.base0B}";
+    NVS_COLOR_WARNING = "#${config.lib.stylix.colors.base0A}";
+    NVS_COLOR_ERROR = "#${config.lib.stylix.colors.base08}";
   };
 }
