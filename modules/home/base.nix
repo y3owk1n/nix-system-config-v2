2e5212a294c73a3b2f45707eeca107ddfa5d11ff
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2026-07-04 15:43:50 +0800

    feat: bump home stateVersion to 26.05

--- modules/home/base.nix
+++ modules/home/base.nix
@@ -2,7 +2,7 @@
   home = {
     inherit username;
     homeDirectory = if pkgs.stdenv.isDarwin then "/Users/${username}" else "/home/${username}";
-    stateVersion = "25.11";
+    stateVersion = "26.05";
     shell.enableShellIntegration = true;
     shellAliases = {
       gg = "lazygit";
