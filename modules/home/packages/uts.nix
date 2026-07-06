ee9dec2e3d359d25704566a917228c49aa8be15c
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2026-07-07 01:43:28 +0800

    feat(uts): add color theming

--- modules/home/packages/uts.nix
+++ modules/home/packages/uts.nix
@@ -1,7 +1,19 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   programs.uts = {
     enable = true;
     package = pkgs.uts-source;
+  };
+
+  home.sessionVariables = {
+    UTS_COLOR_PRIMARY = "#${config.lib.stylix.colors.base0E}";
+    UTS_COLOR_TEXT = "#${config.lib.stylix.colors.base05}";
+    UTS_COLOR_MUTED = "#${config.lib.stylix.colors.base04}";
+    UTS_COLOR_SUBTLE = "#${config.lib.stylix.colors.base03}";
+    UTS_COLOR_BORDER = "#${config.lib.stylix.colors.base02}";
+    UTS_COLOR_ACCENT = "#${config.lib.stylix.colors.base0D}";
+    UTS_COLOR_SUCCESS = "#${config.lib.stylix.colors.base0B}";
+    UTS_COLOR_WARNING = "#${config.lib.stylix.colors.base0A}";
+    UTS_COLOR_ERROR = "#${config.lib.stylix.colors.base08}";
   };
 }
