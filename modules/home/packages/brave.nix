cd136f1cf584ac7af9f93fc600a6aa55075688c0
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2026-07-07 16:57:37 +0800

    feat(brave): add some extensions and flags

--- modules/home/packages/brave.nix
+++ modules/home/packages/brave.nix
@@ -1,3 +1,19 @@
 { pkgs, ... }: {
-  home.packages = [ pkgs.brave ];
+  programs.chromium = {
+    enable = true;
+    package = pkgs.brave;
+
+    extensions = [
+      { id = "fmkadmapgofadopljbjfkapdkoienihi"; } # React DevTools
+      { id = "pejdijmoenmkgeppbflobdenhhabjlaj"; } # iCloud Passwords
+      { id = "ldgfbffkinooeloadekpmfoklnobpien"; } # Raindrop.io
+      { id = "hlepfoohegkhhmjieoechaddaejaokhf"; } # Refined GitHub
+    ];
+
+    commandLineArgs = [
+      "--disable-features=BraveRewards,BraveNews,AIChat,BraveVPN,IPFS,Solana"
+    ];
+
+    dictionaries = [ pkgs.hunspellDictsChromium.en_US ];
+  };
 }
