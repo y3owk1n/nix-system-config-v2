0f575dc67908928e7675ccce2c3b861556720b98
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2026-07-13 12:40:00 +0800

    chore: bump

--- flake.nix
+++ flake.nix
@@ -26,7 +26,9 @@
 
     # Custom Packages & Tools
     mimi.url = "https://flakehub.com/f/y3owk1n/mimi/0.1";
+    # mimi.url = "git+file:/Users/kylewong/Dev/mimi";
     neru.url = "https://flakehub.com/f/y3owk1n/neru/0.1";
+    # neru.url = "git+file:/Users/kylewong/Dev/neru";
     nvs.url = "https://flakehub.com/f/y3owk1n/nvs/0.1";
     uts.url = "https://flakehub.com/f/y3owk1n/uts/0.1";
 
