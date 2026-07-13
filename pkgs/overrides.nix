49bb87c21e9995f06a6e25e7c286fcc1bc06b6b9
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2026-07-13 17:19:09 +0800

    feat: remove kanata overrides

--- pkgs/overrides.nix
+++ pkgs/overrides.nix
@@ -8,26 +8,6 @@ _final: prev: {
     noCheck = true;
   });
 
-  # TODO: Remove this once the nixpkgs version is updated to 1.12.0
-  # BUMP: Latest version refer here -> https://github.com/jtroo/kanata/releases/latest
-  kanata = prev.kanata.overrideAttrs (
-    finalAttrs: _: {
-      cargoHash = "sha256-4UBN4I35ZPPPL68LxxPna9Fs9sATCiwoTbWgHYwqOjs=";
-      src = prev.fetchFromGitHub {
-        owner = "jtroo";
-        repo = "kanata";
-        rev = "v1.12.0";
-        sha256 = "sha256-WjdmjgEMoo3QNqT4yWxaKOkfuRLdNg4Im+V1Hy5vWgY=";
-      };
-      version = "v1.12.0";
-      cargoDeps = prev.rustPlatform.fetchCargoVendor {
-        inherit (finalAttrs) pname src version;
-        hash = finalAttrs.cargoHash;
-      };
-      doInstallCheck = false;
-    }
-  );
-
   # BUMP: Latest commit refer here -> https://github.com/christoomey/vim-tmux-navigator
   tmuxPlugins = prev.tmuxPlugins // {
     vim-tmux-navigator = prev.tmuxPlugins.vim-tmux-navigator.overrideAttrs (_: {
