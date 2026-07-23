d0171806df3a2830613630b514a671b4e6804ef9
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2026-07-23 12:05:13 +0800

    refactor: move uts deps into uts.nix

--- modules/home/packages/uts.nix
+++ modules/home/packages/uts.nix
@@ -1,5 +1,30 @@
 { pkgs, config, ... }:
 {
+  # dependencies for `uts`
+  home.packages = with pkgs; [
+    # Video & audio (used by pact video/audio)
+    ffmpeg
+
+    # PDF (used by pact pdf)
+    ghostscript
+
+    # Image optimization (used by pact image)
+    pngquant
+    optipng
+    jpegoptim
+    gifsicle
+    libwebp # provides cwebp
+    imagemagick
+
+    # Archive algorithms (used by uts archive)
+    zstd
+    brotli
+    xz
+
+    # PDF to image conversion (used by uts convert pdf)
+    poppler # provides pdftoppm
+  ];
+
   programs.uts = {
     enable = true;
     package = pkgs.uts-source;
