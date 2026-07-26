51e70e3ca9e0a3b00d9bb7a38435a65e3e818106
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2026-07-26 10:42:08 +0800

    refactor(neru): clenup

--- modules/home/packages/neru.nix
+++ modules/home/packages/neru.nix
@@ -68,62 +68,6 @@ in
       # ============================================================================
       [hints]
       enabled = false
-      visible_check_enabled = false
-
-      hint_characters = "aeudhtnspyfgcrqjkxbmwvz"
-      include_menubar_hints = true
-      include_dock_hints = true
-      include_nc_hints = true
-      include_stage_manager_hints = true
-      include_pip_hints = true
-      include_screen_capture_hints = true
-      detect_mission_control = true
-      on_mission_control_activated = "hints --action left_click"
-      on_mission_control_deactivated = "idle"
-
-      clickable_roles = [
-        "AXButton",
-        "AXComboBox",
-        "AXCheckBox",
-        "AXRadioButton",
-        "AXLink",
-        "AXPopUpButton",
-        "AXTextField",
-        "AXSlider",
-        "AXTabButton",
-        "AXSwitch",
-        "AXDisclosureTriangle",
-        "AXTextArea",
-        "AXMenuItem",
-        "AXCell",
-        "AXRow",
-      	"AXMenuButton",
-      	"AXGenericElement",
-      ]
-
-      additional_menubar_hints_targets = [
-        "com.apple.TextInputMenuAgent",
-        "com.apple.controlcenter",
-        "com.apple.systemuiserver",
-        "com.y3owk1n.neru",
-      	"com.openai.codex",
-      	"com.google.antigravity",
-      ]
-
-      [[hints.app_configs]]
-      bundle_id = "com.apple.Safari"
-      visible_check_enabled = true
-
-      [hints.additional_ax_support]
-      enable = true
-
-      additional_electron_bundles = ["com.openai.codex", "com.hnc.discord", "com.google.antigravity"]
-      additional_chromium_bundles = []
-      additional_firefox_bundles = []
-      additional_webkit_bundles = []
-
-      [hints.boundary_highlight]
-      enabled = true
 
       # ============================================================================
       # Grid Navigation
@@ -148,7 +92,6 @@ in
       [recursive_grid.ui]
       font_family = "JetBrainsMonoNLNFP-Bold"
       line_width = 1
-      # highlight_color = "#B00A1338"
       highlight_color = "#00000000"
       text_color = "#00000000"
 
@@ -183,20 +126,9 @@ in
       "Ctrl+L" = "action scroll_right"
       "Ctrl+S" = ["action move_mouse --selection", "scroll"]
 
-      # [[recursive_grid.app_configs]]
-      # bundle_id = "com.brave.Browser"
-      # hotkeys = {
-      #   "1" = ["action right_click", "action sleep 0.1", "action feed o p e n enter m d a enter"],
-      #   "2" = ["action right_click", "action sleep 0.1", "action feed o p e n enter s k b enter"],
-      #   "3" = ["action right_click", "action sleep 0.1", "action feed o p e n enter t r a enter"],
-      #   "4" = ["action right_click", "action sleep 0.1", "action feed o p e n enter m a d enter"],
-      #   "5" = ["action right_click", "action sleep 0.1", "action feed o p e n enter w a k enter"],
-      # }
-
       # ============================================================================
       # Scroll
       # ============================================================================
-
       [scroll]
       scroll_step = 100
 
@@ -225,6 +157,12 @@ in
       # Mode Indicator
       # ============================================================================
       [mode_indicator.ui]
+      font_family = "JetBrainsMonoNLNFP-Bold"
+
+      # ============================================================================
+      # Virtual Pointer
+      # ============================================================================
+      [virtual_pointer.ui]
       font_family = "JetBrainsMonoNLNFP-Bold"
     '';
   };
